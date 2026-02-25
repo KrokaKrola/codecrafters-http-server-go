8ddddf372df312369fd93fa34fced66631afba71
KrokaKrola <harlamsan@gmail.com>
2026-02-25 21:43:45 +0200

    codecrafters submit [skip ci]

--- internal/handlers/echo.go
+++ internal/handlers/echo.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"compress/gzip"
 	"strconv"
 
 	"github.com/codecrafters-io/http-server-starter-go/internal/http"
@@ -17,6 +19,19 @@ func HandleEcho() router.HandlerFunc {
 		encodings := http.GetSupportedEncodings(req.Headers)
 		if len(encodings) > 0 {
 			res.Headers.Set("Content-Encoding", "gzip")
+
+			var buf bytes.Buffer
+			zw := gzip.NewWriter(&buf)
+
+			if _, err := zw.Write([]byte(req.Matches[1])); err != nil {
+				res := response.NewResponseByStatusCode(500)
+				zw.Close()
+				return res
+			}
+			zw.Close()
+
+			res.Headers.Set("Content-Length", strconv.Itoa(buf.Len()))
+			res.Body = buf.String()
 		} else {
 			res.Headers.Set("Content-Length", strconv.Itoa(len(req.Matches[1])))
 			res.Body = req.Matches[1]
